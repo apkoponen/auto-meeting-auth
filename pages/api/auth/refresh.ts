a32718bf1269ea1c400015d58d5199156599315a
Ari-Pekka Koponen <ap.koponen@gmail.com>
2020-06-20 21:58:57 +0300

    Fix refresh token endpoint

--- pages/api/auth/refresh.ts
+++ pages/api/auth/refresh.ts
@@ -1,5 +1,6 @@
 import { route } from "../../../src/api/helpers";
 import qs from "qs";
+import Axios from "axios";
 
 export default route().post(async (req, res) => {
   const { refreshToken } = req.body;
@@ -8,19 +9,20 @@ export default route().post(async (req, res) => {
     res.status(400).end();
   }
 
-  const result = await fetch("https://oauth2.googleapis.com/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: qs.stringify({
+  const result = await Axios.post(
+    "https://oauth2.googleapis.com/token",
+    qs.stringify({
       refresh_token: refreshToken,
       client_id: process.env.GOOGLE_CLIENT_ID,
-      client_secret: process.env.GOOGLE_CLIENT_ID,
-      redirect_uri: "",
-      grant_type: "authorization_code",
+      client_secret: process.env.GOOGLE_CLIENT_SECRET,
+      grant_type: "refresh_token",
     }),
-  });
+    {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    }
+  );
 
-  res.status(200).json(result);
+  res.status(200).json(result.data);
 });
